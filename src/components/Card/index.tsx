fa2d2a3b73f1afad8009828d90ea13aee8ab4d8a
OtavioFilipe <darksnack16@gmail.com>
2022-10-18 20:54:23 -0300

    add icons

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
 import { Feather } from "@expo/vector-icons";
 import React, { useRef } from "react";
+import { Text } from "react-native";
 import { Swipeable } from "react-native-gesture-handler";
 import { TodoProps } from "../../screens/Home";
 
@@ -22,9 +23,15 @@ const Card: React.FC<CardProps> = ({ data }: CardProps) => {
           <Feather name="check-circle" size={20} color="#fff" />
         </Styles.Success>
       )}
-      renderRightActions={() => <Styles.Delete></Styles.Delete>}
+      renderRightActions={() => (
+        <Styles.Delete>
+          <Feather name="trash" size={20} color="#fff" />
+        </Styles.Delete>
+      )}
     >
-      <Styles.Container></Styles.Container>
+      <Styles.Container>
+        <Text>{data.name}</Text>
+      </Styles.Container>
     </Swipeable>
   );
 };
